4a43f829968bdcfb9fd338eec988d39b1393312c
Javier Goncalves <jagi.ccs@gmail.com>
2019-11-30 07:28:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,10 +18,10 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   base_hash = {
-    :railroads => {pieces: 4, 
+    :railroads => {pieces: 4,
       :rent_in_dollars => {key:"", key2:"", key3:"", key4:""},
       :names => ""
-    
+
     }
   }
 end
